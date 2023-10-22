dc956c6f74eaf587fb3278cb80d50a5537554008
Voinash Denys <desdenisandqwerty@gmail.com>
2023-10-22 15:56:37 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const elements = {
+  start: document.querySelector('[data-start]'),
+  stop: document.querySelector('[data-stop]'),
+};
+let timerId = null;
+elements.stop.disabled = true;
 
+elements.start.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    document.body.style.background = getRandomHexColor();
+  }, 1000);
+  elements.start.disabled = true;
+  elements.stop.disabled = false;
+});
+
+elements.stop.addEventListener('click', () => {
+  clearInterval(timerId);
+  elements.start.disabled = false;
+  elements.stop.disabled = true;
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
