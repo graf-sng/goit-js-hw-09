786c7c21f5d9b21441d58537526989dfc63a97a4
Voinash Denys <desdenisandqwerty@gmail.com>
2023-10-23 18:06:21 +0300

    task 3

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -68,3 +68,23 @@ function convertMs(ms) {
 function addLeadingZero(value) {
   return String(value).padStart(2, '0');
 }
+const fetchUserFromServer = username => {
+  return new Promise((resolve, reject) => {
+    console.log(`Fetching data for ${username}`);
+
+    setTimeout(() => {
+      // Change value of isSuccess variable to simulate request status
+      const isSuccess = 1;
+
+      if (isSuccess) {
+        resolve('success value');
+      } else {
+        reject('error');
+      }
+    }, 2000);
+  });
+};
+
+fetchUserFromServer('Mango')
+  .then(user => console.log(user))
+  .catch(error => console.error(error));
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', handlerSubmit);
+
+function handlerSubmit(e) {
+  e.preventDefault();
+  let {
+    delay: { value: delay },
+    step: { value: step },
+    amount: { value: amount },
+  } = e.currentTarget.elements;
+
+  delay = Number(delay);
+  step = Number(step);
+  amount = Number(amount);
+
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += step;
   }
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
